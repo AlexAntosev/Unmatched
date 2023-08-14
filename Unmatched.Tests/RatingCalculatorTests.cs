10b373139247fd6721b65709da0813db0b0ca20e
Andrii Antosiev <gibson12ant@gmail.com>
2023-08-14 12:27:48 +0300

    Finish RatingCalculator

--- Unmatched.Tests/RatingCalculatorTests.cs
+++ Unmatched.Tests/RatingCalculatorTests.cs
@@ -4,7 +4,6 @@ using Unmatched.DataInitialization;
 using Unmatched.Entities;
 using Unmatched.Repositories;
 using Unmatched.Services;
-using Xunit.Sdk;
 
 namespace Unmatched.Tests;
 
@@ -15,11 +14,11 @@ public class RatingCalculatorTests
     {
         var heroRepository = CreateMockHeroRepository();
         var heroes = heroRepository.Query().ToList();
-        
+
         var ratingRepository = CreateMockRatingRepository(heroes);
-        
+
         var calculator = new RatingCalculator(ratingRepository, heroRepository);
-        
+
         var drSatId = heroes.First(x => x.Name.Equals(HeroNames.DrSattler)).Id;
         var daredevilId = heroes.First(x => x.Name.Equals(HeroNames.Daredevil)).Id;
         var robinHoodId = heroes.First(x => x.Name.Equals(HeroNames.RobinHood)).Id;
@@ -34,19 +33,84 @@ public class RatingCalculatorTests
         var bloodyMaryId = heroes.First(x => x.Name.Equals(HeroNames.BloodyMary)).Id;
 
         AssertMatch(await calculator.CalculateAsync(
-                new Fighter()
-                    { HeroId = drSatId, HpLeft = 8, CardsLeft = 10, SidekickHpLeft = 1, Turn = 2, IsWinner = true },
-                new Fighter()
-                    { HeroId = daredevilId, HpLeft = 0, CardsLeft = 5, SidekickHpLeft = 0, Turn = 1, IsWinner = false }),
-            new HeroMatchPoints() { HeroId = drSatId, Points = 420 },
-            new HeroMatchPoints() { HeroId = daredevilId, Points = -225 });
+                new Fighter
+                {
+                    HeroId = drSatId, HpLeft = 8, CardsLeft = 10, SidekickHpLeft = 1, Turn = 2, IsWinner = true
+                },
+                new Fighter
+                {
+                    HeroId = daredevilId, HpLeft = 0, CardsLeft = 5, SidekickHpLeft = 0, Turn = 1, IsWinner = false
+                }),
+            new HeroMatchPoints { HeroId = drSatId, Points = 420 },
+            new HeroMatchPoints { HeroId = daredevilId, Points = -225 });
+        
+        AssertMatch(await calculator.CalculateAsync(
+                new Fighter
+                {
+                    HeroId = robinHoodId, HpLeft = 8, CardsLeft = 13, SidekickHpLeft = 1, Turn = 2, IsWinner = true
+                },
+                new Fighter
+                {
+                    HeroId = ingenId, HpLeft = 0, CardsLeft = 16, SidekickHpLeft = 0, Turn = 1, IsWinner = false
+                }),
+            new HeroMatchPoints { HeroId = robinHoodId, Points = 336 },
+            new HeroMatchPoints { HeroId = ingenId, Points = 0 });
+        
+        AssertMatch(await calculator.CalculateAsync(
+                new Fighter
+                {
+                    HeroId = rexId, HpLeft = 17, CardsLeft = 16, SidekickHpLeft = 0, Turn = 1, IsWinner = true
+                },
+                new Fighter
+                {
+                    HeroId = sherlokHolmesId, HpLeft = 0, CardsLeft = 18, SidekickHpLeft = 7, Turn = 2, IsWinner = false
+                }),
+            new HeroMatchPoints { HeroId = rexId, Points = 316 },
+            new HeroMatchPoints { HeroId = sherlokHolmesId, Points = -100 });
+        
+        AssertMatch(await calculator.CalculateAsync(
+                new Fighter
+                {
+                    HeroId = invisibleManId, HpLeft = 6, CardsLeft = 7, SidekickHpLeft = 0, Turn = 2, IsWinner = true
+                },
+                new Fighter
+                {
+                    HeroId = sindbadId, HpLeft = 0, CardsLeft = 9, SidekickHpLeft = 5, Turn = 1, IsWinner = false
+                }),
+            new HeroMatchPoints { HeroId = invisibleManId, Points = 360 },
+            new HeroMatchPoints { HeroId = sindbadId, Points = -165 });
+        
+        AssertMatch(await calculator.CalculateAsync(
+                new Fighter
+                {
+                    HeroId = raptorsId, HpLeft = 8, CardsLeft = 11, SidekickHpLeft = 0, Turn = 1, IsWinner = true
+                },
+                new Fighter
+                {
+                    HeroId = draculaId, HpLeft = 0, CardsLeft = 10, SidekickHpLeft = 1, Turn = 2, IsWinner = false
+                }),
+            new HeroMatchPoints { HeroId = raptorsId, Points = 496 },
+            new HeroMatchPoints { HeroId = draculaId, Points = -190 });
+        
+        AssertMatch(await calculator.CalculateAsync(
+                new Fighter
+                {
+                    HeroId = medusaId, HpLeft = 5, CardsLeft = 2, SidekickHpLeft = 0, Turn = 1, IsWinner = true
+                },
+                new Fighter
+                {
+                    HeroId = bloodyMaryId, HpLeft = 0, CardsLeft = 1, SidekickHpLeft =0, Turn = 2, IsWinner = false
+                }),
+            new HeroMatchPoints { HeroId = medusaId, Points = 272 },
+            new HeroMatchPoints { HeroId = bloodyMaryId, Points = -195 });
     }
 
-    private void AssertMatch(IEnumerable<HeroMatchPoints> matchPoints, HeroMatchPoints winnerExpectedPoints, HeroMatchPoints looserExpectedPoints)
+    private void AssertMatch(IEnumerable<HeroMatchPoints> matchPoints, HeroMatchPoints winnerExpectedPoints,
+        HeroMatchPoints looserExpectedPoints)
     {
         var actualWinnerPoints = matchPoints.First(x => x.HeroId.Equals(winnerExpectedPoints.HeroId)).Points;
         var actualLooserPoints = matchPoints.First(x => x.HeroId.Equals(looserExpectedPoints.HeroId)).Points;
-        
+
         Assert.Equal(winnerExpectedPoints.Points, actualWinnerPoints);
         Assert.Equal(looserExpectedPoints.Points, actualLooserPoints);
     }
@@ -54,54 +118,40 @@ public class RatingCalculatorTests
     private IRatingRepository CreateMockRatingRepository(IEnumerable<Hero> heroes)
     {
         var mockRepo = new Mock<IRatingRepository>();
-        
-        var drSatId = heroes.First(x => x.Name.Equals(HeroNames.DrSattler)).Id;
-        var daredevilId = heroes.First(x => x.Name.Equals(HeroNames.Daredevil)).Id;
-        var robinHoodId = heroes.First(x => x.Name.Equals(HeroNames.RobinHood)).Id;
-        var ingenId = heroes.First(x => x.Name.Equals(HeroNames.Ingen)).Id;
-        var rexId = heroes.First(x => x.Name.Equals(HeroNames.TRex)).Id;
-        var sherlokHolmesId = heroes.First(x => x.Name.Equals(HeroNames.SherlokHolmes)).Id;
-        var invisibleManId = heroes.First(x => x.Name.Equals(HeroNames.InvisibleMan)).Id;
-        var sindbadId = heroes.First(x => x.Name.Equals(HeroNames.Sindbad)).Id;
-        var raptorsId = heroes.First(x => x.Name.Equals(HeroNames.Raptors)).Id;
-        var draculaId = heroes.First(x => x.Name.Equals(HeroNames.Dracula)).Id;
-        var medusaId = heroes.First(x => x.Name.Equals(HeroNames.Medusa)).Id;
-        var bloodyMaryId = heroes.First(x => x.Name.Equals(HeroNames.BloodyMary)).Id;
 
-        var ratings = new List<Rating>()
+        var ratings = new List<Rating>
         {
-            new Rating() { HeroId = drSatId, Points = 0 },
-            new Rating() { HeroId = daredevilId, Points = 700 },
-            new Rating(){HeroId = robinHoodId,Points = 1500}
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.DrSattler)).Id, Points = 0 },
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.Daredevil)).Id, Points = 700 },
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.RobinHood)).Id, Points = 1500 },
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.Ingen)).Id, Points = 0 },
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.TRex)).Id, Points = 0 },
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.SherlokHolmes)).Id, Points = 100 },
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.InvisibleMan)).Id, Points = 400 },
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.Sindbad)).Id, Points = 1200 },
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.Raptors)).Id, Points = 0 },
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.Dracula)).Id, Points = 1000 },
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.Medusa)).Id, Points = 1300 },
+            new() { HeroId = heroes.First(x => x.Name.Equals(HeroNames.BloodyMary)).Id, Points = 1700 }
         };
-        mockRepo.Setup(x => x.GetByHeroIdAsync(It.IsAny<Guid>())).Returns((Guid heroId) => Task.FromResult(ratings.First(x => x.HeroId == heroId)));
+        mockRepo.Setup(x => x.GetByHeroIdAsync(It.IsAny<Guid>()))
+            .Returns((Guid heroId) => Task.FromResult(ratings.First(x => x.HeroId == heroId)));
 
-        // mockRepo.Setup(x => x.GetByIdAsync(drSatId)).Returns(Task.FromResult(new Rating(){HeroId = drSatId,Points = 0}));
-        // mockRepo.Setup(x => x.GetByIdAsync(daredevilId)).Returns(Task.FromResult(new Rating(){HeroId = daredevilId,Points = 700}));
-        // mockRepo.Setup(x => x.GetByIdAsync(robinHoodId)).Returns(Task.FromResult(new Rating(){HeroId = robinHoodId,Points = 1500}));
-        // mockRepo.Setup(x => x.GetByIdAsync(ingenId)).Returns(Task.FromResult(new Rating(){HeroId = ingenId,Points = 0}));
-        // mockRepo.Setup(x => x.GetByIdAsync(rexId)).Returns(Task.FromResult(new Rating(){HeroId = rexId,Points = 0}));
-        // mockRepo.Setup(x => x.GetByIdAsync(sherlokHolmesId)).Returns(Task.FromResult(new Rating(){HeroId = sherlokHolmesId,Points = 100}));
-        // mockRepo.Setup(x => x.GetByIdAsync(invisibleManId)).Returns(Task.FromResult(new Rating(){HeroId = invisibleManId,Points = 400}));
-        // mockRepo.Setup(x => x.GetByIdAsync(sindbadId)).Returns(Task.FromResult(new Rating(){HeroId = sindbadId,Points = 1200}));
-        // mockRepo.Setup(x => x.GetByIdAsync(raptorsId)).Returns(Task.FromResult(new Rating(){HeroId = raptorsId,Points = 0}));
-        // mockRepo.Setup(x => x.GetByIdAsync(draculaId)).Returns(Task.FromResult(new Rating(){HeroId = draculaId,Points = 1000}));
-        // mockRepo.Setup(x => x.GetByIdAsync(medusaId)).Returns(Task.FromResult(new Rating(){HeroId = medusaId,Points = 1300}));
-        // mockRepo.Setup(x => x.GetByIdAsync(bloodyMaryId)).Returns(Task.FromResult(new Rating(){HeroId = bloodyMaryId,Points = 1700}));
         return mockRepo.Object;
     }
 
     private IHeroRepository CreateMockHeroRepository()
     {
         var mockSidekickRepository = new Mock<ISidekickRepository>();
-        mockSidekickRepository.Setup(x => x.Query()).Returns((new SidekicksDataInitializer(null)).GetEntities());
+        mockSidekickRepository.Setup(x => x.Query()).Returns(new SidekicksDataInitializer(null).GetEntities());
 
         var heroDataInitializer = new HeroesDataInitializer(null, mockSidekickRepository.Object);
 
         var mockHeroRepository = new Mock<IHeroRepository>();
         var heroes = heroDataInitializer.GetEntities();
         mockHeroRepository.Setup(x => x.Query()).Returns(heroes);
-        mockHeroRepository.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).Returns((Guid id) => Task.FromResult(heroes.First(x => x.Id ==id))); 
+        mockHeroRepository.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
+            .Returns((Guid id) => Task.FromResult(heroes.First(x => x.Id == id)));
 
         return mockHeroRepository.Object;
     }
--- Unmatched/Services/RatingCalculator.cs
+++ Unmatched/Services/RatingCalculator.cs
@@ -5,34 +5,35 @@ namespace Unmatched.Services;
 
 public class RatingCalculator : IRatingCalculator
 {
-    private readonly IRatingRepository _ratingRepository;
     private readonly IHeroRepository _heroRepository;
+    private readonly IRatingRepository _ratingRepository;
 
     public RatingCalculator(IRatingRepository ratingRepository, IHeroRepository heroRepository)
     {
         _ratingRepository = ratingRepository;
         _heroRepository = heroRepository;
     }
+
     public async Task<IEnumerable<HeroMatchPoints>> CalculateAsync(Fighter fighter, Fighter opponent)
     {
         var winnerFighter = fighter.IsWinner ? fighter : opponent;
         var looserFighter = fighter.IsWinner ? opponent : fighter;
-        
+
         var matchContext = new MatchContext(
             await _heroRepository.GetByIdAsync(winnerFighter.HeroId),
-            await _heroRepository.GetByIdAsync(looserFighter.HeroId), 
-            winnerFighter, 
+            await _heroRepository.GetByIdAsync(looserFighter.HeroId),
+            winnerFighter,
             looserFighter,
             (await _ratingRepository.GetByHeroIdAsync(winnerFighter.HeroId))?.Points ?? 0,
             (await _ratingRepository.GetByHeroIdAsync(looserFighter.HeroId))?.Points ?? 0);
-        
-        var fighterMatchPoints = new HeroMatchPoints()
+
+        var fighterMatchPoints = new HeroMatchPoints
         {
             HeroId = winnerFighter.HeroId,
             Points = CalculateForWinner(matchContext)
         };
-        
-        var opponentMatchPoints = new HeroMatchPoints()
+
+        var opponentMatchPoints = new HeroMatchPoints
         {
             HeroId = looserFighter.HeroId,
             Points = CalculateForLooser(matchContext)
@@ -43,25 +44,49 @@ public class RatingCalculator : IRatingCalculator
 
     private int CalculateForLooser(MatchContext matchContext)
     {
-        int forSidekickHp=0 ;
-        int forWinnerHpLeft =0;
+        var maxLooserSidekicksHp = matchContext.LooserReferenceHero.Sidekicks?.Sum(x => x.Hp * x.Count) ?? 0;
+        var maxWinnerSidekicksHp = matchContext.WinnerReferenceHero.Sidekicks?.Sum(x => x.Hp * x.Count) ?? 0;
+
+        var forSidekickHp = maxLooserSidekicksHp == 0
+            ? 50
+            : Convert.ToInt32(
+                50.0 * (1.0 - Math.Round((double)matchContext.LooserFighter.SidekickHpLeft / maxLooserSidekicksHp, 1,
+                    MidpointRounding.ToPositiveInfinity)));
+        var forWinnerHpLeft = Convert.ToInt32(150 *
+                                              Math.Round(
+                                                  (double)(matchContext.WinnerFighter.HpLeft +
+                                                           matchContext.WinnerFighter.SidekickHpLeft) /
+                                                  (matchContext.WinnerReferenceHero.Hp + maxWinnerSidekicksHp), 1,
+                                                  MidpointRounding.ToPositiveInfinity));
         var result = -(100 + forSidekickHp + forWinnerHpLeft);
-        return matchContext.LooserPointsBeforeMatch + result > 0 ? result : 0;
+        return matchContext.LooserPointsBeforeMatch + result > 0 ? result : -matchContext.LooserPointsBeforeMatch;
     }
 
     private int CalculateForWinner(MatchContext matchContext)
     {
         var maxLooserSidekicksHp = matchContext.LooserReferenceHero.Sidekicks?.Sum(x => x.Hp * x.Count) ?? 0;
         var maxWinnerSidekicksHp = matchContext.WinnerReferenceHero.Sidekicks?.Sum(x => x.Hp * x.Count) ?? 0;
-        int forLooserSidekick = maxLooserSidekicksHp == 0
+
+        var forLooserSidekick = maxLooserSidekicksHp == 0
             ? 40
             : (int)Math.Round(
                 40.0 * (1.0 - Math.Round((double)matchContext.LooserFighter.SidekickHpLeft / maxLooserSidekicksHp, 1,
                     MidpointRounding.ToPositiveInfinity)), 0);
-        
-        int forCardsLeft =Convert.ToInt32(80*Math.Round(matchContext.WinnerFighter.CardsLeft/(0.8*matchContext.WinnerReferenceHero.DeckSize),1,MidpointRounding.ToPositiveInfinity));
-        int forHandicap = Convert.ToInt32((matchContext.LooserPointsBeforeMatch > matchContext.WinnerPointsBeforeMatch) ? 100*((matchContext.LooserPointsBeforeMatch-matchContext.WinnerPointsBeforeMatch)/500): 0);
-        int forHpLeft =Convert.ToInt32(80*Math.Round((double)(matchContext.WinnerFighter.HpLeft+matchContext.WinnerFighter.SidekickHpLeft)/(matchContext.WinnerReferenceHero.Hp +maxWinnerSidekicksHp),1,MidpointRounding.ToPositiveInfinity));;
+
+        var forCardsLeft = Convert.ToInt32(80 *
+                                           Math.Round(
+                                               matchContext.WinnerFighter.CardsLeft /
+                                               (0.8 * matchContext.WinnerReferenceHero.DeckSize), 1,
+                                               MidpointRounding.ToPositiveInfinity));
+        var forHandicap = Convert.ToInt32(matchContext.LooserPointsBeforeMatch > matchContext.WinnerPointsBeforeMatch
+            ? 100 * ((matchContext.LooserPointsBeforeMatch - matchContext.WinnerPointsBeforeMatch) / 500)
+            : 0);
+        var forHpLeft = Convert.ToInt32(80 *
+                                        Math.Round(
+                                            (double)(matchContext.WinnerFighter.HpLeft +
+                                                     matchContext.WinnerFighter.SidekickHpLeft) /
+                                            (matchContext.WinnerReferenceHero.Hp + maxWinnerSidekicksHp), 1,
+                                            MidpointRounding.ToPositiveInfinity));
         var result = 200 + forLooserSidekick + forCardsLeft + forHandicap + forHpLeft;
         return result;
     }
