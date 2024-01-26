ac23a2abf7c857d985dd631eca5ee09a3c0eced1
Oleksandr Antosiev <oleksandr_antosiev@epam.com>
2024-01-26 16:40:25 +0200

    Add colors

--- Unmatched/Constants/Colors.cs
+++ Unmatched/Constants/Colors.cs
@@ -9,4 +9,10 @@ public static class Colors
     public const string Planned = "#d3d3d4";
     
     public const string Default = "rgba(255,255,255,1)";
+
+    public const string Champion = "Gold";
+    
+    public const string SecondPlace = "Silver";
+
+    public const string Leader = "rgb(205, 127, 50)";
 }
